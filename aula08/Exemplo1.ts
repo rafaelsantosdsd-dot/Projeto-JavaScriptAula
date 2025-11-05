bc9a56439e6e7a9231c9bfdcb10903de06cac336
Rafael Dantas <rafael.santos.dsd@gmail.com>
2025-11-05 13:46:52 -0300

    Exemplos de métodos

--- aula08/Exemplo1.ts
+++ aula08/Exemplo1.ts
@@ -1,5 +1,30 @@
-import { Stack } from "../aula07/Stack";
+//~import { Stack } from "../aula07/Stack";
 
-const pilha = new Stack<string>()
+// const pilha = new Stack<string>()
 
-pilha.push("")
+// pilha.push("")
+
+//DECLARANDO A FUNÇÃO
+export function somar(a: number, b: number): number{
+    // Lógica da função
+
+    let resultado: number = a + b 
+
+    return resultado
+}
+
+export function logar(usuario: number | string): void{
+    if (typeof(usuario) === "number" )
+        console.log("\nVocê está acessando pelo celular");
+    else
+        console.log("\nVocê está acessando pelo e-mail");
+}
+
+//CHAMANDO A FUNÇÃO
+let valor: number = somar(7, 12)
+let valor1: number = somar(6,11)
+
+console.log("O resultado da sua adição é: "+valor)
+console.log("O resultado da sua adição é: "+valor1)
+
+logar("Roberta@generation")
---
+++ aula08/Teste.ts
@@ -0,0 +1,10 @@
+import { somar } from "./Exemplo1";
+import { logar } from "./Exemplo1";
+
+let valor: number = somar(7, 12)
+let valor1: number = somar(6,11)
+
+console.log("O resultado da sua adição é: "+valor)
+console.log("O resultado da sua adição é: "+valor1)
+
+logar("Roberta@generation")
